fe9d5c77189dd5c4243c2f6279f143ddc1ee1467
Felix <felix.haas@stud.th-rosenheim.de>
2024-05-14 22:17:06 +0200

    added visual response to clicks and dragover events while handling edge-cases

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -12,6 +12,28 @@ interface Props {
 
 function Square({ black, position, handleMove, children } : PropsWithChildren<Props>) {
 
+    const handleMouseDown = (e: React.MouseEvent) => {
+        if (e.button === 0) {
+            e.currentTarget.classList.add("hovering");
+        } else if (e.button === 1) { // prevents "hovering" from sticking in edge-cases
+            e.currentTarget.classList.remove("hovering");
+        }
+    }
+
+    const handleMouseUp = (e: React.MouseEvent) => {
+        e.currentTarget.classList.remove("hovering");
+    }
+
+    const handleOnMouseLeave = (e: React.MouseEvent) => {
+        e.currentTarget.classList.remove("hovering");
+    }
+
+    const handleDoubleClick = (e: React.MouseEvent) => {
+        // TODO: find and highlight all valid positions of current piece
+        console.log("clickety-click!");
+        console.log(e.currentTarget);
+    }
+
     const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
         const startSquare = e.currentTarget;
         const startPosition = startSquare.getAttribute("data-position");
@@ -20,11 +42,17 @@ function Square({ black, position, handleMove, children } : PropsWithChildren<Pr
             e.dataTransfer.setData("startPosition", startPosition);
     };
 
-    const enableDrop = (e: React.DragEvent<HTMLDivElement>) => {
-        e.preventDefault();
+    const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+        e.currentTarget.classList.add("hovering");
+        e.preventDefault(); // enable drop
     };
 
+    const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+        e.currentTarget.classList.remove("hovering");
+    }
+
     const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+        e.currentTarget.classList.remove("hovering");
         const startPosition = e.dataTransfer.getData("startPosition");
         const endPosition = e.currentTarget.getAttribute("data-position");
         // endPosition should never be null, but just in case
@@ -36,8 +64,13 @@ function Square({ black, position, handleMove, children } : PropsWithChildren<Pr
         <div
             className={`square ${black ? "brown" : "beige"}`}
             data-position={position}
+            onMouseDown={e => handleMouseDown(e)}
+            onMouseUp={e => handleMouseUp(e)}
+            onMouseLeave={e => handleOnMouseLeave(e)}
+            onDoubleClick={e => handleDoubleClick(e)}
             onDragStart={e => handleDragStart(e)}
-            onDragOver={e => enableDrop(e)}
+            onDragOver={e => handleDragOver(e)}
+            onDragLeave={e => handleDragLeave(e)}
             onDrop={e => handleDrop(e)}
         >
             { children }
