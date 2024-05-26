88bf6842e76b86a9be0ecc7f50072f9372703363
Felix <felix.haas@stud.th-rosenheim.de>
2024-05-26 15:48:44 +0200

    added minimal express backend. use "npm run dev2" (yes 2).

--- src/components/StartMenu.tsx
+++ src/components/StartMenu.tsx
@@ -1,18 +1,28 @@
-import { useNavigate } from 'react-router-dom';
-
 import './StartMenu.css';
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 
 function StartMenu() {
     const navigate = useNavigate();
+
+    const [text, setText] = useState("Click me!");
+    const test_server_connection = async function() {
+        let response = await axios.get("/message");
+        console.log(response);
+        let new_text = response.data;
+        console.log(new_text);
+        setText(new_text);
+    }
 
     return(
         <div className='container'>
             <button onClick={() => navigate('/play')}>
                 Play
             </button>
-            <button >
-                Something else
+            <button onClick={test_server_connection}>
+                {text}
             </button>
         </div>
     );
