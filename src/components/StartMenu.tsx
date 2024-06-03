680e50ef2300e349ab5558301f3ee17d27e6a349
jjjoshmmm <107488249+jjjoshmmm@users.noreply.github.com>
2024-06-03 18:20:16 +0200

    board looks nice

--- src/components/StartMenu.tsx
+++ src/components/StartMenu.tsx
@@ -55,81 +55,19 @@ import React from 'react';
 
 function StartMenu() {
     const navigate = useNavigate();
-    const [text, setText] = useState("Click me!");
+    
+    
 
+    const [text, setText] = useState("Click me!");
     const test_server_connection = async function() {
-        let response = await axios.get("/message");
+        let response = await axios.get("/api/hello");
         console.log(response);
         let new_text = response.data;
         console.log(new_text);
         setText(new_text);
     }
 
-    {/*return (
-        <div className='container'>
-            <div className='left-column'>
-                <div className='card'>
-                    <h1 className='title'>Super Duper Nice Chess</h1>
-                    <button onClick={() => navigate('/play')}>
-                        Play Local Game
-                    </button>
-                    <button onClick={() => navigate('/online')}>
-                        Play Online Game
-                    </button>
-                    <button onClick={() => navigate('/login')}>
-                        Login
-                    </button>
-                    <button onClick={test_server_connection}>
-                        {text}
-                    </button>
-                </div>
-            </div>
-            <div className='right-column'>
-                <div className='card'>
-                    <Lottie animationData={animationData} />
-                </div>
-            </div>
-        </div>
     
-
-
-
-    );*/}
-    
-    
-    
-    // Create an array to determine positions of button containers
-    /*const isButtonContainer = (index: number) => {
-        return index >= 10 && index < 15;
-    };
-
-    return (
-        <div className='container'>
-            {Array.from({ length: 25 }).map((_, index) => (
-                isButtonContainer(index) ? (
-                    <div className='grid-item button-container' key={index}>
-                        <button onClick={() => navigate('/play')}>
-                            Play Local Game
-                        </button>
-                        <button onClick={() => navigate('/online')}>
-                            Play Online Game
-                        </button>
-                        <button onClick={() => navigate('/login')}>
-                            Login
-                        </button>
-                        <button onClick={test_server_connection}>
-                            {text}
-                        </button>
-                    </div>
-                    
-                ) : (
-                    <div className='grid-item' key={index}>
-                        <Lottie animationData={animationData} />
-                    </div>
-                )
-            ))}
-        </div>
-    );*/
 
     // Define an array of logos to use
     const logo1 = animationKnight;
@@ -166,7 +104,7 @@ function StartMenu() {
                         {index === 11 && <button onClick={() => navigate('/play')}>Play Local Game</button>}
                         {index === 13 && <button onClick={() => navigate('/online')}>Play Online Game</button>}
                         {index === 12 && <button onClick={() => navigate('/login')}>Login</button>}
-                        {index === 10 && <button onClick={test_server_connection}>Test Server</button>}
+                        {index === 10 && <button onClick={test_server_connection}>{text}</button>}
                         {index === 14 && <button onClick={() => navigate('/another')}>Settings</button>}
                     </div>
                 ) : (
