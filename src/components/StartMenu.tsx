c20e6c06b554b64a0b8d7353624010347dbc0b55
Felix <felix.haas@stud.th-rosenheim.de>
2024-06-04 14:58:48 +0200

    "npm run dev2" is now "npm run dev" again

--- src/components/StartMenu.tsx
+++ src/components/StartMenu.tsx
@@ -1,46 +1,7 @@
-/*import './StartMenu.css';
-import axios from "axios";
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-
-
-function StartMenu() {
-    const navigate = useNavigate();
-
-    const [text, setText] = useState("Click me!");
-    const test_server_connection = async function() {
-        let response = await axios.get("/api/hello");
-        console.log(response);
-        let new_text = response.data;
-        console.log(new_text);
-        setText(new_text);
-    }
-
-    return(
-        <div className='container'>
-            <button onClick={() => navigate('/play')}>
-                Play
-            </button>
-            <button onClick={test_server_connection}>
-                {text}
-            </button>
-            <button onClick={() => navigate('/user/new')}>
-                Create new User
-            </button>      
-            <button onClick={() => navigate('/game/new')}>
-                Create new Game
-            </button>  
-        </div>
-    );
-}
-
-export default StartMenu;*/
-
-
 import './StartMenu2.css';
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Lottie, { LottieRefCurrentProps} from 'lottie-react'
 /*import animationBoard from '../assets/Animation-board.json'*/
 import animationKnight from '../assets/Animation-knight.json'
@@ -48,17 +9,10 @@ import animationRook from '../assets/Animation - Rook.json'
 import React from 'react';
 
 
-
-
-
-
-
 function StartMenu() {
-    const navigate = useNavigate();
-    
-    
 
-    const [text, setText] = useState("Click me!");
+    const navigate = useNavigate();
+    const [text, setText] = useState("Test server");
     const test_server_connection = async function() {
         let response = await axios.get("/api/hello");
         console.log(response);
@@ -66,8 +20,6 @@ function StartMenu() {
         console.log(new_text);
         setText(new_text);
     }
-
-    
 
     // Define an array of logos to use
     const logo1 = animationKnight;
@@ -83,33 +35,28 @@ function StartMenu() {
     };
 
     const lottieRef = React.useRef<LottieRefCurrentProps>(null)
-    
+
     useEffect(() => {
         if (lottieRef.current) {
             lottieRef.current?.setSpeed(0.5);
         }
         console.log(lottieRef);
     }, [lottieRef]);
-    
-
-    
-
-    
 
     return (
         <div className='container'>
             {Array.from({ length: 25 }).map((_, index) => (
                 isButtonContainer(index) ? (
                     <div className='grid-item' key={index}>
-                        {index === 11 && <button onClick={() => navigate('/play')}>Play Local Game</button>}
-                        {index === 13 && <button onClick={() => navigate('/online')}>Play Online Game</button>}
-                        {index === 12 && <button onClick={() => navigate('/login')}>Login</button>}
                         {index === 10 && <button onClick={test_server_connection}>{text}</button>}
-                        {index === 14 && <button onClick={() => navigate('/another')}>Settings</button>}
+                        {index === 11 && <button onClick={() => navigate('/play')}>Play Local Game</button>}
+                        {index === 12 && <button onClick={() => navigate('/online')}>Play Online Game</button>}
+                        {index === 13 && <button onClick={() => navigate('/login')}>Login</button>}
+                        {index === 14 && <button onClick={() => navigate('/settings')}>Settings</button>}
                     </div>
                 ) : (
                     <div className='grid-item' key={index}>
-                        
+
                         <Lottie lottieRef={lottieRef} animationData={logos[index % logos.length]}/>
                         {/*<img src={logos[index % logos.length]} alt="Chess Logo" className='logo' />*/}
                     </div>
