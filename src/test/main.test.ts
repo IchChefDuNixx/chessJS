34d9b97c9f1fa7bbfaae5bbc979f51c28c8dd8cd
Felix <felix.haas@stud.th-rosenheim.de>
2024-06-21 00:00:14 +0200

    added a few more tests

--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -1,1 +1,42 @@
-// https://github.com/mswjs/examples/tree/main/examples/with-vitest
+import axios from "axios";
+import { expect, it } from "vitest"
+
+it("pings the server", async () => {
+    const response = await axios.get("http://localhost:5173/api/hello");
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual("Hello from express!");
+});
+
+it("resets the game", async () => {
+    const response = await axios.post("http://localhost:5173/api/restart_game");
+    expect(response.status).toBe(200);
+    expect(response.data).toMatch("");
+});
+
+it("gets possible moves", async () => {
+    let response = await axios.post("http://localhost:5173/api/possible_moves", { index: 0 });
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual([]);
+
+    response = await axios.post("http://localhost:5173/api/possible_moves", { index: 6 });
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual(expect.arrayContaining([21, 23]));
+
+    response = await axios.post("http://localhost:5173/api/possible_moves", { index: 9 });
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual(expect.arrayContaining([17, 25]));
+
+    response = await axios.post("http://localhost:5173/api/possible_moves", { index: 54 });
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual(expect.arrayContaining([38, 46]));
+
+    response = await axios.post("http://localhost:5173/api/possible_moves", { index: 63 });
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual([]);
+
+    await expect(axios.post("http://localhost:5173/api/possible_moves", { index: -1 })).rejects.toThrowError();
+
+    await expect(axios.post("http://localhost:5173/api/possible_moves", { index: 100 })).rejects.toThrowError();
+});
+
+it("validates a specific move", async () => {});
